01886d22e6667fee806d6377230fbd6c4efab0b7
aditi1986 <103111483+aditi1986@users.noreply.github.com>
2023-03-18 19:06:55 +0530

    Add files via upload

---
+++ GroupAssignment1/SuperDepartment.java
@@ -0,0 +1,21 @@
+package GroupAssignment;
+
+public class SuperDepartment {
+
+	public String departmentName() {
+		return "Super Department";
+	}
+
+	public String getTodaysWork() {
+		return "No work as of now";
+	}
+
+	public String isTodayaHoliday() {
+		return "Today is not a Holiday";
+	}
+
+	public String getWorkDeadline() {
+		return "Nil";
+	}
+
+}
